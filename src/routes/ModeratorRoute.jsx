594961da94226a5a8fb758c63edeb7051e192e60
FokhrulSabel <fokhrulislam316@gmail.com>
2026-02-14 20:01:56 +0600

    Add Moderator Route

--- src/routes/ModeratorRoute.jsx
+++ src/routes/ModeratorRoute.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import useAuth from "../hooks/useAuth";
+import useRole from "../hooks/useRole";
+import Loader from "../components/Loader/Loader";
 
-const ModeratorRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const ModeratorRoute = ({ children }) => {
+  const { loading } = useAuth();
+  const { role, isLoading } = useRole();
+
+  if (loading || isLoading) {
+    return <Loader></Loader>;
+  }
+
+  if (role !== "moderator") {
+    return;
+  }
+  return children;
 };
 
 export default ModeratorRoute;
